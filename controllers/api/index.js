f5a8c7d4f8b7be37de7d64e31217db92284e9b78
Betsy <betsy.griffith12@gmail.com>
2022-08-05 13:19:19 -0400

    added post-routes.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes.js');
 
+//these are the params that will show in the url: ex www.tech-blog.com/users
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
