847ab34110a11a192b103b2bfccfda7e4f9dba0e
Betsy <betsy.griffith12@gmail.com>
2022-08-07 10:54:10 -0400

    added comments to the posts and user

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes');
 
 //these are the params that will show in the url: ex www.tech-blog.com/users
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
